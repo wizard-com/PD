a4941a7acc690e95dd23d31125f47d77e76b44b8
18045945 <aungmoethet2000@gmail.com>
2020-02-15 22:47:42 +0800

    Data visualization and dialog

---
+++ app/src/main/java/com/example/pdthird/BmiDataActivity.java
@@ -0,0 +1,35 @@
+package com.example.pdthird;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BmiDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bmi_data_layout);
+        Pie pie = AnyChart.pie();
+
+        List<DataEntry> data = new ArrayList<>();
+        data.add(new ValueDataEntry("John", 10000));
+        data.add(new ValueDataEntry("Jake", 12000));
+        data.add(new ValueDataEntry("Peter", 18000));
+
+        pie.data(data);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie);
+    }
+
+
+}
